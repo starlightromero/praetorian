c332260da7dc449ac437cdd962da15927c11266d
Starlight Romero <starlightromero@gmail.com>
2020-11-22 10:11:48 -0800

    added navlink to account

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 import classes from './LoginForm.module.css'
 
 const LoginForm = props => {
@@ -13,7 +14,11 @@ const LoginForm = props => {
         <Label for='pasword'>Password</Label>
         <Input type='password' name='password' id='password' placeholder='Enter your password' />
       </FormGroup>
-      <Button color='primary'>Login</Button>
+      <NavLink
+        exact
+        to='/account'>
+        <Button color='primary'>LOGIN</Button>
+      </NavLink>
     </Form>
   )
 }
