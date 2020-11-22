d89f7036c695cdb68ea987688404cf4b2a900633
Starlight Romero <starlightromero@gmail.com>
2020-11-22 13:52:45 -0800

    styled shield

--- src/Containers/Welcome/Welcome.js
+++ src/Containers/Welcome/Welcome.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Container, Button } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
+import { ReactComponent as Shield } from '../../assets/shield-large.svg'
+import classes from './Welcome.module.css'
 
 const Welcome = props => {
   return (
     <Container className='h-100 d-flex flex-column justify-content-center align-items-center'>
+      <Shield className={classes.Shield} />
       <h1 className='display-1 text-white'>PRAETORIAN</h1>
       <h3 className='text-white'>In a busy world protect what matters most - yourself.</h3>
       <div className='w-100 d-flex flex-row justify-content-between align-items-center'>
