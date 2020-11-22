d7c13badb2b27f06acc10e2a1b74f92bfbf4a814
Starlight Romero <starlightromero@gmail.com>
2020-11-22 13:05:35 -0800

    align-items-center

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -9,7 +9,7 @@ const Login = props => {
   return (
     <Container className='h-100 d-flex flex-column justify-content-center align-items-center'>
       <BackButton />
-      <Row className='w-100'>
+      <Row className='w-100 align-items-center'>
         <Col md='6'>
           <LoginForm />
         </Col>
