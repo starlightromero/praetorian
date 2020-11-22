8041a8606d3ab6d941771de9d964a27c4627bc3f
Starlight Romero <starlightromero@gmail.com>
2020-11-22 13:06:05 -0800

    added align-items-center

--- src/Containers/Apply/Apply.js
+++ src/Containers/Apply/Apply.js
@@ -7,7 +7,7 @@ const Apply = () => {
   return (
     <Container className='h-100 d-flex flex-column justify-content-center align-items-center'>
       <BackButton />
-      <Row className='w-100'>
+      <Row className='w-100 align-items-center'>
         <Col md='6'>
           <ApplyForm />
         </Col>
