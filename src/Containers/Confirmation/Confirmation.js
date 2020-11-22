b60c223e7e2cdce260b55950d538082c5eaccab7
Starlight Romero <starlightromero@gmail.com>
2020-11-22 13:53:17 -0800

    corrected casing

--- src/Containers/Confirmation/Confirmation.js
+++ src/Containers/Confirmation/Confirmation.js
@@ -4,8 +4,8 @@ import { Container } from 'reactstrap'
 const Confirmation = props => {
   return (
     <Container className='h-100 d-flex flex-column justify-content-center align-items-center'>'>
-      <h1 className='text-grey'>THANK YOU FOR APPLYING</h1>
-      <h1 className='text-grey'>WE WILL BE IN TOUCH</h1>
+      <h1 className='text-grey'>Thank you for applying</h1>
+      <h1 className='text-grey'>We will be in touch</h1>
     </Container>
   )
 }
