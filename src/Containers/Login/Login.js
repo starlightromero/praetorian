741cf16a8627b18a171af0d0c30cb27e6c966d58
Starlight Romero <starlightromero@gmail.com>
2020-11-22 10:36:05 -0800

    added back button to login

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
-import { ReactComponent as Lock } from '../../assets/lock.svg'
 import classes from './Login.module.css'
+import { ReactComponent as Lock } from '../../assets/lock.svg'
+import BackButton from '../../Components/BackButton/BackButton'
 import LoginForm from '../../Components/LoginForm/LoginForm'
 
 const Login = props => {
   return (
     <Container className='h-100 d-flex flex-column justify-content-center align-items-center'>
+      <BackButton />
       <Row className='w-100'>
         <Col md='6'>
           <LoginForm />
         </Col>
         <Col md='6' className='text-center'>
           <h1 className='text-white display-3'>LOGIN</h1>
-          <Lock className='Lock' />
+          <Lock className={classes.Lock} />
         </Col>
       </Row>
     </Container>
