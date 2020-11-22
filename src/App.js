f8ed6a49ef671a519c9893b5058f323455dd3554
Starlight Romero <starlightromero@gmail.com>
2020-11-22 09:58:36 -0800

    added routes for welcome and login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Containers/Home/Home'
+import Welcome from './Containers/Welcome/Welcome'
+import Login from './Containers/Login/Login'
 import Apply from './Containers/Apply/Apply'
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
       <Switch>
+        <Route exact path='/' component={Welcome} />
+        <Route exact path='/login' component={Login} />
         <Route exact path='/apply' component={Apply} />
-        <Route exact path='/' component={Home} />
       </Switch>
     </Router>
   )
