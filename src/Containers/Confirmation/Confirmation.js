20009028666cc4cce6d0d776d2e38f0b474904d2
Starlight Romero <starlightromero@gmail.com>
2020-11-22 10:19:44 -0800

    created confirmation component

---
+++ src/Containers/Confirmation/Confirmation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Confirmation = props => {
+  return (
+    <div>
+      <h1 className='text-secondary'>THANK YOU FOR APPLYING</h1>
+      <h1 className='text-secondary'>WE WILL BE IN TOUCH</h1>
+    </div>
+  )
+}
+
+export default Confirmation
