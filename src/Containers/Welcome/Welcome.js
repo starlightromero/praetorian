07b413a4024e8c37a9a8e75f67672d3dde9b1b4f
Starlight Romero <starlightromero@gmail.com>
2020-11-22 09:57:44 -0800

    added welcome container

---
+++ src/Containers/Welcome/Welcome.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Container, Button } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
+
+const Welcome = props => {
+  return (
+    <Container className='h-100 d-flex flex-column justify-content-center align-items-center'>
+      <h1 className='display-1 text-white'>PRAETORIAN</h1>
+      <h3 className='text-white'>In a busy world protect what matters most - yourself.</h3>
+      <div className='w-100 d-flex flex-row justify-content-between align-items-center'>
+        <NavLink
+          exact
+          to='/login'>
+          <Button outline color='light'>LOGIN</Button>
+        </NavLink>
+        <NavLink
+          exact
+          to='/learn-more'>
+          <Button outline color='light'>LEARN MORE</Button>
+        </NavLink>
+        <NavLink
+          exact
+          to='/apply'>
+          <Button outline color='light'>APPLY</Button>
+        </NavLink>
+      </div>
+    </Container>
+  )
+}
+
+export default Welcome
