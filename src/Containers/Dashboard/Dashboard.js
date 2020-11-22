ea8a26aa666d70cf62028619bcf7291ea10ca047
Starlight Romero <starlightromero@gmail.com>
2020-11-22 13:53:10 -0800

    added shield and layout

--- src/Containers/Dashboard/Dashboard.js
+++ src/Containers/Dashboard/Dashboard.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import { ReactComponent as Shield } from '../../assets/shield.svg'
 
 const Dashboard = props => {
   return (
-    <div>Dashboard</div>
+    <Container>
+      <Row>
+        <Col md='6'>
+          <div>
+            <h5>Praetorian's Name</h5>
+            <h5>Status</h5>
+          </div>
+        </Col>
+        <Col md='6' className='text-center'>
+          <h1 className='text-white display-3'>Account</h1>
+          <Shield />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
