c2458164163b5fe7f1ccbd778bcdf58eabe0a27e
Starlight Romero <starlightromero@gmail.com>
2020-11-22 11:50:00 -0800

    created applyform component

---
+++ src/Components/ApplyForm/ApplyForm.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import classes from './ApplyForm.module.css'
+import api from '../../api.js'
+import Loader from '../../UI/Loader/Loader'
+
+class ApplyForm extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    loading: false
+  }
+
+  applyHandler = event => {
+    event.preventDefault()
+    alert('hi')
+    this.setState({ loading: true })
+    const { name, email, password } = this.state
+    api.post('/user', {
+      name: 'Starlight',
+      email: 'starlight@starlight.com',
+      password: 'password',
+    }).then(response => {
+      console.log(response.data)
+      this.setState({ loading: false })
+      this.props.history.push('/confirmation')
+    }).catch(error => {
+      this.setState({ loading: false })
+    })
+  }
+  
+  render () {
+    let form = (
+      <Form className={classes.Form}>
+        <FormGroup>
+          <Label for='name'>Name</Label>
+          <Input type='name' name='name' id='name' placeholder='Enter your name' />
+        </FormGroup>
+        <FormGroup>
+          <Label for='email'>Email</Label>
+          <Input type='email' name='email' id='email' placeholder='Enter your email' />
+        </FormGroup>
+        <FormGroup>
+          <Label for='password'>Password</Label>
+          <Input type='password' name='password' id='password' placeholder='Enter a password' />
+        </FormGroup>
+        <FormGroup>
+          <Label for='select'>Select</Label>
+          <Input type='select' name='select' id='select'>
+            <option>Praetorian</option>
+            <option>2</option>
+          </Input>
+        </FormGroup>
+        <Button color='primary' onClick={this.applyHandler}>Apply</Button>
+      </Form>
+    )
+
+    if (this.state.loading === true) {
+      form = <Loader />
+    }
+    
+    return (
+      <>
+        {form}
+      </>
+    )
+  }
+}
+
+export default ApplyForm
