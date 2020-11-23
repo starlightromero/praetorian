fc9dd6800dde3d0ef7aa2b022973df10e9c0b7b0
Starlight Romero <starlightromero@gmail.com>
2020-11-22 20:22:07 -0800

    refactored component as class

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
 import classes from './LoginForm.module.css'
 
-const LoginForm = props => {
-  return (
-    <Form className={classes.Form}>
-      <FormGroup>
-        <Label for='email' className='text-grey'>Email</Label>
-        <Input type='email' name='email' id='email' placeholder='Enter your email' />
-      </FormGroup>
-      <FormGroup>
-        <Label for='pasword' className='text-grey'>Password</Label>
-        <Input type='password' name='password' id='password' placeholder='Enter your password' />
-      </FormGroup>
-      <NavLink
-        exact
-        to='/account'>
-        <Button color='primary' className='w-100'>Login</Button>
-      </NavLink>
-    </Form>
-  )
+class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    loading: false
+  }
+
+  
+  render () {
+    return (
+      <Form className={classes.Form}>
+        <FormGroup>
+          <Label for='email' className='text-grey'>Email</Label>
+          <Input type='email' name='email' id='email' placeholder='Enter your email' />
+        </FormGroup>
+        <FormGroup>
+          <Label for='pasword' className='text-grey'>Password</Label>
+          <Input type='password' name='password' id='password' placeholder='Enter your password' />
+        </FormGroup>
+        <NavLink
+          exact
+          to='/account'>
+          <Button color='primary' className='w-100'>Login</Button>
+        </NavLink>
+      </Form>
+    )
+  }
 }
 
 export default LoginForm
