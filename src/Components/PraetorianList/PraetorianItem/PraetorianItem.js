52e9a38f0699ffe43ef1c89c30341e6178f9cc49
Starlight Romero <starlightromero@gmail.com>
2020-11-23 12:40:06 -0800

    added onClick attribute

--- src/Components/PraetorianList/PraetorianItem/PraetorianItem.js
+++ src/Components/PraetorianList/PraetorianItem/PraetorianItem.js
@@ -3,7 +3,9 @@ import classes from './PraetorianItem.module.css'
 
 const PraetorianItem = props => {
   return (
-    <div className={classes.Item}>
+    <div
+      className={classes.Item}
+      onClick={() => props.clicked(props.name)}>
       <h6>{props.name}</h6>
       <h6>{props.status}</h6>
     </div>
