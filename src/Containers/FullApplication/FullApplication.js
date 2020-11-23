114606ca7dc441a3dbf88e2d996452159afff06b
Starlight Romero <starlightromero@gmail.com>
2020-11-23 15:33:29 -0800

    added FullApplication container

---
+++ src/Containers/FullApplication/FullApplication.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import { ReactComponent as Papers } from '../../assets/papers.svg'
+import PraetorianApplication from '../../Components/PraetorianApplication/PraetorianApplication'
+import BackButton from '../../Components/BackButton/BackButton'
+
+const FullApplication = props => {
+  return (
+    <Container className='h-100 d-flex flex-column justify-content-center align-items-center'>
+      <BackButton {...props} />
+      <Row className='w-100 align-items-center'>
+        <Col md='6'>
+          <PraetorianApplication {...props} />
+        </Col>
+        <Col md='6' className='text-center'>
+          <h1 className='text-white display-3 mb-5'>Apply</h1>
+          <Papers />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default FullApplication
