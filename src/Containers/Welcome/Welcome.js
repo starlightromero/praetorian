abe875eafc87feb5361c74999f2095155d4b60c4
Starlight Romero <starlightromero@gmail.com>
2020-11-23 08:54:35 -0800

    changed NavLink to Link

--- src/Containers/Welcome/Welcome.js
+++ src/Containers/Welcome/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Button } from 'reactstrap'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { ReactComponent as Shield } from '../../assets/shield-large.svg'
 import classes from './Welcome.module.css'
 
@@ -11,21 +11,21 @@ const Welcome = props => {
       <h1 className='display-1 text-white'>PRAETORIAN</h1>
       <h3 className='text-white'>In a busy world protect what matters most - yourself.</h3>
       <div className='w-100 d-flex flex-row justify-content-between align-items-center'>
-        <NavLink
+        <Link
           exact
           to='/login'>
           <Button outline color='light'>LOGIN</Button>
-        </NavLink>
-        <NavLink
+        </Link>
+        <Link
           exact
           to='/learn-more'>
           <Button outline color='light'>LEARN MORE</Button>
-        </NavLink>
-        <NavLink
+        </Link>
+        <Link
           exact
           to='/apply'>
           <Button outline color='light'>APPLY</Button>
-        </NavLink>
+        </Link>
       </div>
     </Container>
   )
