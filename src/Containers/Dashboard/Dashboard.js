b64c5a09250d4c033757bc59a5725510086d92ab
Starlight Romero <starlightromero@gmail.com>
2020-11-22 16:10:51 -0800

    added PraetorianList component

--- src/Containers/Dashboard/Dashboard.js
+++ src/Containers/Dashboard/Dashboard.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import { ReactComponent as Shield } from '../../assets/shield.svg'
+import PraetorianList from '../../Components/PraetorianList/PraetorianList'
 
 const Dashboard = props => {
   return (
-    <Container>
-      <Row>
-        <Col md='6'>
-          <div>
-            <h5>Praetorian's Name</h5>
-            <h5>Status</h5>
-          </div>
-        </Col>
+    <Container fluid className='h-100'>
+      <Row className='h-100'>
         <Col md='6' className='text-center'>
+          <PraetorianList />
+        </Col>
+        <Col md='6' className='text-center d-flex flex-column justify-content-center align-items-center'>
           <h1 className='text-white display-3'>Account</h1>
           <Shield />
         </Col>
