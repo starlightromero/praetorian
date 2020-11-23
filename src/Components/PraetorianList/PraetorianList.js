7b0ed68f4d966c8eae993b161ca205a36287f977
Starlight Romero <starlightromero@gmail.com>
2020-11-23 10:13:55 -0800

    generate praetorian list from backend data

--- src/Components/PraetorianList/PraetorianList.js
+++ src/Components/PraetorianList/PraetorianList.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './PraetorianList.module.css'
 import PraetorianItem from './PraetorianItem/PraetorianItem'
+import api from '../../api'
 
-const PraetorianList = props => {
-  return (
-    <div className={classes.List}>
-      <PraetorianItem />
-      <PraetorianItem />
-      <PraetorianItem />
-    </div>
-  )
+class PraetorianList extends Component {
+  state = {
+    praetorians: []
+  }
+
+  componentWillMount () {
+    api.get('/praetorians').then(res => {
+      this.setState({'praetorians': res.data.praetorians})
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  render () {
+    let praetorianList = (
+      this.state.praetorians.map(praetorian => (
+        <PraetorianItem
+          key={praetorian} 
+          name={praetorian}
+          status='Active' />
+      ))
+    )
+    
+    return (
+      <div className={classes.List}>
+        {praetorianList}
+      </div>
+    )
+  }
 }
 
 export default PraetorianList
