d88541490f1fec1059692ae6f2d8a5eb60ee527a
Starlight Romero <starlightromero@gmail.com>
2020-11-23 10:41:16 -0800

    refactored dashboard to hold state insteaf of list

--- src/Components/PraetorianList/PraetorianList.js
+++ src/Components/PraetorianList/PraetorianList.js
@@ -1,37 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import classes from './PraetorianList.module.css'
 import PraetorianItem from './PraetorianItem/PraetorianItem'
-import api from '../../api'
 
-class PraetorianList extends Component {
-  state = {
-    praetorians: []
-  }
+const PraetorianList = props => {
+  let praetorianList = (
+    props.praetorians.map(praetorian => (
+      <PraetorianItem
+        key={praetorian}
+        name={praetorian}
+        status='Active' />
+    ))
+  )
 
-  componentWillMount () {
-    api.get('/praetorians').then(res => {
-      this.setState({'praetorians': res.data.praetorians})
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-
-  render () {
-    let praetorianList = (
-      this.state.praetorians.map(praetorian => (
-        <PraetorianItem
-          key={praetorian} 
-          name={praetorian}
-          status='Active' />
-      ))
-    )
-    
-    return (
-      <div className={classes.List}>
-        {praetorianList}
-      </div>
-    )
-  }
+  return (
+    <div className={classes.List}>
+      {praetorianList}
+    </div>
+  )
 }
 
 export default PraetorianList
--- src/Containers/Dashboard/Dashboard.js
+++ src/Containers/Dashboard/Dashboard.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import { ReactComponent as Shield } from '../../assets/shield.svg'
 import { ReactComponent as Add } from '../../assets/add-button.svg'
+import api from '../../api'
 import PraetorianList from '../../Components/PraetorianList/PraetorianList'
 
-const Dashboard = props => {
-  return (
-    <Container fluid className='h-100'>
-      <Row className='h-100'>
-        <Col md='6' className='text-center'>
-          <PraetorianList />
-        </Col>
-        <Col md='6' className='text-center d-flex flex-column justify-content-center align-items-center'>
-          <h1 className='text-white display-3'>Account</h1>
-          <Shield />
-        </Col>
-      </Row>
-    </Container>
-  )
+class Dashboard extends Component {
+  state = {
+    praetorians: []
+  }
+
+  componentWillMount () {
+    api.get('/praetorians').then(res => {
+      this.setState({'praetorians': res.data.praetorians})
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  render() {
+    return (
+      <Container fluid className='h-100'>
+        <Row className='h-100'>
+          <Col md='6' className='text-center'>
+            <PraetorianList praetorians={this.state.praetorians} />
+          </Col>
+          <Col md='6' className='text-center d-flex flex-column justify-content-center align-items-center'>
+            <h1 className='text-white display-3'>Account</h1>
+            <Shield />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default Dashboard
