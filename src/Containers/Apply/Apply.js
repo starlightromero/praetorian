db4759bf7f6c7ee03e5863fe676025a1271fb5fe
Starlight Romero <starlightromero@gmail.com>
2020-11-23 08:55:14 -0800

    passed props to BackButton and ApplyForm

--- src/Containers/Apply/Apply.js
+++ src/Containers/Apply/Apply.js
@@ -7,10 +7,10 @@ import ApplyForm from '../../Components/ApplyForm/ApplyForm'
 const Apply = props => {
   return (
     <Container className='h-100 d-flex flex-column justify-content-center align-items-center'>
-      <BackButton />
+      <BackButton {...props} />
       <Row className='w-100 align-items-center'>
         <Col md='6'>
-          <ApplyForm />
+          <ApplyForm {...props} />
         </Col>
         <Col md='6' className='text-center'>
           <h1 className='text-white display-3 mb-5'>Apply</h1>
