b9321feae53e52605e3737b1412b239eed89c3cf
Starlight Romero <starlightromero@gmail.com>
2020-11-23 17:45:53 -0800

    refactored to use Modal component

--- src/Components/DetailView/DetailView.js
+++ src/Components/DetailView/DetailView.js
@@ -1,25 +1,12 @@
 import React from 'react'
-import classes from './DetailView.module.css'
-import { ReactComponent as Close } from '../../assets/close.svg'
+import Modal from '../UI/Modal/Modal'
 
 const DetailView = props => {
-  let praetorian = <h1>No Active Praetorian</h1>
-
-  if (props.praetorian) {
-    praetorian = <h1>{props.praetorian}</h1>
-  }
-
   return (
-    <div
-      className={classes.View}
-      style={{
-        transform: props.show ? 'translateX(0)' : 'translateX(50vw)',
-        opacity: props.show ? 1 : 0
-      }}>
-      <Close className={classes.Close} onClick={props.clicked} />
+    <Modal {...props}>
       <h1>Detail View</h1>
-      {praetorian}
-    </div>
+      <h1>{props.praetorian}</h1>
+    </Modal>
   )
 }
 
