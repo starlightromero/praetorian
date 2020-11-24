0fe6020f2acfff5a7d327f3680bb2b5a218901ea
Starlight Romero <starlightromero@gmail.com>
2020-11-23 18:17:34 -0800

    changed account to user

--- api/praetorian/models.py
+++ api/praetorian/models.py
@@ -3,7 +3,7 @@ from praetorian import db
 
 
 class User(db.Model):
-    """Account database class."""
+    """User database class."""
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
