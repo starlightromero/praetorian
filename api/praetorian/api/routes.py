e740ea750319538f999e22b90b97415e9460a538
Starlight Romero <starlightromero@gmail.com>
2020-11-23 18:47:32 -0800

    separated praetorian and executive routes

--- api/praetorian/api/routes.py
+++ api/praetorian/api/routes.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, request, jsonify
 from flask_praetorian import auth_required
 from praetorian import db, guard
-from praetorian.models import User
+from praetorian.models import Praetorian, Executive
 
 api = Blueprint("api", __name__)
 
@@ -10,46 +10,91 @@ api = Blueprint("api", __name__)
 @api.route("/praetorians", methods=["GET"])
 def get_praetorians():
     """Return all Praetorians."""
-    praetorians = User.query.filter_by(account_type="praetorian").all()
+    praetorians = Praetorian.query.filter_by(account_type="praetorian").all()
     praetorian_list = []
     for praetorian in praetorians:
         praetorian_list.append(praetorian.name)
     return jsonify({"praetorians": praetorian_list})
 
 
-@api.route("/user", methods=["POST"])
-def create_user():
-    """Create User in database."""
+@api.route("/praetorian", methods=["POST"])
+def create_praetorian():
+    """Create a new Praetorian in database."""
     email = request.json.get("email")
-    user = User.query.filter_by(email=email).first()
-    if user:
-        return jsonify({"error": "User already exists"})
+    praetorian = Praetorian.query.filter_by(email=email).first()
+    if praetorian:
+        return jsonify({"error": "Praetorian already exists"})
     name = request.json.get("name")
     password = request.json.get("password")
     account_type = request.json.get("account")
     hashed_password = guard.hash_password(password)
-    user = User(
+    praetorian = Praetorian(
         name=name,
         email=email,
         password=hashed_password,
         account_type=account_type,
     )
-    db.session.add(user)
+    db.session.add(praetorian)
     db.session.commit()
     return (
-        jsonify({"message": f"{user.name} has been created"}),
+        jsonify({"message": f"{praetorian.name} has been created"}),
         201,
     )
 
 
-@api.route("/login", methods=["POST"])
-def login_user():
-    """Login User and return token and username."""
+@api.route("/praetorian", methods=["PATCH"])
+def update_praetorian():
+    """Update a Praetorian in database."""
     email = request.json.get("email")
-    user = User.query.filter_by(email=email).first()
-    if not user:
-        return jsonify({"error": "User does not exists"})
+    praetorian = Praetorian.query.filter_by(email=email).first()
+    if not praetorian:
+        return jsonify({"error": "Praetorian does not exist"})
+    praetorian.experience = request.json.get("experience")
+    praetorian.phone = request.json.get("phone")
+    praetorian.address = request.json.get("address")
+    praetorian.city = request.json.get("city")
+    praetorian.state = request.json.get("state")
+    praetorian.travel = request.json.get("travel")
+    praetorian.background = request.json.get("background")
+    db.session.commit()
+    return (
+        jsonify({"message": f"{praetorian.name} has been updated"}),
+        201,
+    )
+
+
+def create_executive():
+    """Create a new Executive in database."""
+    email = request.json.get("email")
+    executive = Executive.query.filter_by(email=email).first()
+    if executive:
+        return jsonify({"error": "Executive already exists"})
+    name = request.json.get("name")
     password = request.json.get("password")
-    user = guard.authenticate(email, password)
-    token = guard.encode_jwt_token(user)
-    return jsonify({"token": token, "username": user.name}), 200
+    account_type = request.json.get("account")
+    hashed_password = guard.hash_password(password)
+    executive = Executive(
+        name=name,
+        email=email,
+        password=hashed_password,
+        account_type=account_type,
+    )
+    db.session.add(executive)
+    db.session.commit()
+    return (
+        jsonify({"message": f"{executive.name} has been created"}),
+        201,
+    )
+
+
+@api.route("/executive/login", methods=["POST"])
+def login_user():
+    """Login Executive and return token and username."""
+    email = request.json.get("email")
+    executive = Executive.query.filter_by(email=email).first()
+    if not executive:
+        return jsonify({"error": "Executive does not exists"})
+    password = request.json.get("password")
+    executive = guard.authenticate(email, password)
+    token = guard.encode_jwt_token(executive)
+    return jsonify({"token": token, "username": executive.name}), 200
