4e2b6116cfdb5d2d4f5fe5e37c427bf4543656b4
Starlight Romero <starlightromero@gmail.com>
2020-11-23 18:17:27 -0800

    added button to remove praetorian

--- src/Components/DetailView/DetailView.js
+++ src/Components/DetailView/DetailView.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from 'reactstrap'
 import Modal from '../UI/Modal/Modal'
 
 const DetailView = props => {
@@ -6,6 +7,9 @@ const DetailView = props => {
     <Modal {...props}>
       <h1>Detail View</h1>
       <h1>{props.praetorian}</h1>
+      <Button color='primary' onClick={props.removeClicked}>
+        Remove {props.praetorian}
+      </Button>
     </Modal>
   )
 }
