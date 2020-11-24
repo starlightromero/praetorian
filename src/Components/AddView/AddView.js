610a44bd43ae449b1d73d7aa05fd37fc34a44583
Starlight Romero <starlightromero@gmail.com>
2020-11-23 17:46:05 -0800

    created AddView component

---
+++ src/Components/AddView/AddView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Modal from '../UI/Modal/Modal'
+
+const AddView = props => {
+  return (
+    <Modal {...props}>
+      <h1>Add View</h1>
+    </Modal>
+  )
+}
+
+export default AddView
