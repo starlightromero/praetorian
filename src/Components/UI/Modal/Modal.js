26e5b09699b892f05a36dbd837c7da2150205eb2
Starlight Romero <starlightromero@gmail.com>
2020-11-30 11:17:54 -0800

    Reconfigured Modal component

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,18 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classes from './Modal.module.css'
-import { ReactComponent as Close } from '../../../assets/close.svg'
+import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = props => {
-  return (
+const Modal = props => (
+  <>
+    <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
     <div
       className={classes.Modal}
       style={{
-        transform: props.show ? 'translateX(0)' : 'translateX(50vw)'
-      }}>
-      <Close className={classes.Close} onClick={props.clicked} />
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? 1 : 0
+      }}
+      >
       {props.children}
     </div>
-  )
+  </>
+)
+
+Modal.propTypes = {
+  show: PropTypes.bool,
+  modalClosed: PropTypes.func,
+  children: PropTypes.object
 }
 
-export default Modal
+export default React.memo(
+  Modal,
+  (prevProps, nextProps) => (
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+  )
+)
