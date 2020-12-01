3069d67d6ab22884b2892cc9cb1999601ef72b40
Starlight Romero <starlightromero@gmail.com>
2020-11-30 20:30:14 -0800

    added state and use effect

--- src/Components/AddView/AddView.js
+++ src/Components/AddView/AddView.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import api from '../../api'
 import SideMenu from '../UI/SideMenu/SideMenu'
+import PraetorianList from '../PraetorianList/PraetorianList'
 
 const AddView = props => {
+  const [ praetorians, setPraetorians ] = useState([])
+
+  useEffect(() => {
+    api.get('/praetorians').then(res => {
+      setPraetorians(res.data.praetorians)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   return (
-    <SideMenu {...props}>
-      <h1>Add View</h1>
+    <SideMenu
+      clicked={props.close}
+      show={props.show}>
+      <h1 className='mt-3 text-secondary'>Available Praetorians</h1>
+      <PraetorianList praetorians={praetorians} clicked={props.addPraetorian} />
     </SideMenu>
   )
 }
