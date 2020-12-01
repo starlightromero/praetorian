728b2501906e1b1d8b84ad497acae31bddae9291
Starlight Romero <starlightromero@gmail.com>
2020-11-30 20:30:41 -0800

    added div and styling around addicon

--- src/Components/AddButton/AddButton.js
+++ src/Components/AddButton/AddButton.js
@@ -3,7 +3,9 @@ import { ReactComponent as AddIcon } from '../../assets/add-button.svg'
 import classes from './AddButton.module.css'
 
 const AddButton = props => (
-  <AddIcon className={classes.AddButton} onClick={props.clicked} />
+  <div className={classes.AddContainer}>
+    <AddIcon className={classes.AddButton} onClick={props.clicked} />
+  </div>
 )
 
 export default AddButton
